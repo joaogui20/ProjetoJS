e9dc258582a1955260758143a8bb11804ea63f5c
João Guilherme Marques Andrade <39714603+joaogui20@users.noreply.github.com>
2022-01-13 08:53:41 -0400

    Primeiro programa JS em que testo as variáveis

---
+++ primeiroPrograma.js
@@ -0,0 +1,14 @@
+console.log("Meu primeiro programa JS");
+
+const idade = 25;
+console.log(idade);
+console.log(idade*2);
+console.log(idade-2);
+console.log(idade+2);
+console.log(idade/2);
+
+const idadeMulti = idade * 5;
+console.log(idadeMulti);
+
+nome = "Rafael";
+console.log("O " + nome + " é o nome do meu fofinho :3");
