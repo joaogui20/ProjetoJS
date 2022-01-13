00d6d3874e826affce33efff3f320ce982286189
João Guilherme Marques Andrade <39714603+joaogui20@users.noreply.github.com>
2022-01-13 09:59:30 -0400

    Add files via upload

---
+++ conversaoTipos.js
@@ -0,0 +1,12 @@
+console.log("Conversão de Tipos");
+
+console.log("Ano" + 2022);
+console.log("2" + "2");
+
+console.log(parseInt("2") + parseInt("2")); // Converter texto para número
+
+console.log("10" / "2");
+console.log("João" / "2"); // Not a number
+
+console.log(6.5);
+console.log(6,5);
---
+++ operacoesAritmeticas.js
@@ -0,0 +1,9 @@
+console.log("Operações Aritméticas");
+
+console.log(10 + 80 * 2);
+console.log(2 + 2);
+console.log((10 + 80) * 2);
+
+console.log("Ano" + 2022);
+console.log("2" + "2");
+
