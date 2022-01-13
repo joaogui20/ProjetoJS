e3affeb3c6210094191856d9b29373316286d196
João Guilherme Marques Andrade <39714603+joaogui20@users.noreply.github.com>
2022-01-13 09:07:01 -0400

    Estudando as boas práticas das variáveis

---
+++ variaveis.js
@@ -0,0 +1,12 @@
+// Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+
+// Nesse código será estudado as variáveis
+idade = 25;
+Idade = 25;
+
+console.log(idade);
+console.log(Idade); // É opcional usar em maiúscula ou minúscula, mas melhor opção é declarar em minúscula
+
+
+
